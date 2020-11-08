12d8bef08d41823cd4ca7b73f4d1606689cb912a
nithish <nithishvishal007@gmail.com>
2020-11-08 13:11:41 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieparser = require('cookie-parser');
+const {User} = require('./models/User');
+const config = require('./config/key');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test',
-                {useNewUrlParser:true},{uuseUnifiedTopology: true}).then(() => console.log("DB Connected")).catch(err => console.log(err));
 
-app.get('/',(req,res) => {
-    res.send('Hello Nithish');
+mongoose.connect(config.mongoURI,
+     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => console.log('DB is connected'))
+                           .catch(err => console.error(err));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieparser());
+
+app.post("/api/users/register",(req,res) => {
+    const user = new User(req.body);
+    user.save((err,doc) => {
+        if(err) return res.json ({success:false,err})
+        return res.status(200).json({
+            success:true
+        });
+    });  
+});
+
+app.post("/api/user/login",(req,res) => {
+    //find the email
+    user.findOne({email:req.body.email},(err,user) =>{
+        if(!user)
+        return res.json({
+            loginSuccess:false,
+            message:"Auth failed,email not found"
+        });
+    
+
+    //comparepassword
+    user.comparePassword(req.body.password,(err,isMatch) => {
+        if(!isMatch){
+            return res.json({
+                loginSuccess:false,
+                message:"Wrong password"})   
+            }
+    });
+    //generate token
+    user.generateToken((err,user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie("nivi",user.token)
+            .status(200)
+    })
+    })
 })
 
-
-
-
-
-app.listen(3000,(req,res) => {
-    console.log("Server has started!!!!");
-})
+const PORT = process.env.PORT || 5000;
+app.listen( PORT , () => console.log(`Server has started on port ${PORT}`));
